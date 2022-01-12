3f5bd72c5378c3a2ce02e2af77b1ee0fff818752
Facundo-Buchert <96147040+Facundo-Buchert@users.noreply.github.com>
2022-01-12 01:36:25 -0300

    mejoras-según-lvl1
    Se aplicaron las mejoras que se disponían en el archivo "nivel 1" de la carpeta de instrucciones.

--- js-xmas-edition-master/js/main.js
+++ js-xmas-edition-master/js/main.js
@@ -1,28 +1,16 @@
-// :)
-// Producido por Sir Sink (Pica)
-
 
 const $form = document.querySelector("#letter-to-santa")
-
-const $name = $form.name.value
-const town = $form.town.value
-const giftDescription = $form["gift-description"].value
-
-
-
-console.log($name)
-console.log(town)
-console.log(giftDescription)
 
 function validateName(name) {
     if (name.length === 0){
         return "Este campo debe tener al menos un caracter"
     }
-
-    if (name.length > 50) {
-        return "Este campo no puede tener mas de 50 caracteres"
+    if(!/^[a-z]+$/i.test(name)){
+        return "Este campo solo puede tener letras"
     }
-
+    if (name.length > 50) {
+        return "Este campo no puede tener más de 50 caracteres"
+    }
     return ""
 }
 
@@ -30,10 +18,13 @@ function validateGiftDescription (giftDescription) {
     if (giftDescription.length === 0) {
         return "Este campo debe tener al menos un caracter"
     }
-
-    if (giftDescription.length > 200) {
-        return "Este campo no puede tener mas de 200 caracteres"
+    if(!/^[a-z0-9 ]+$/i.test(giftDescription)){
+        return "Este campo solo puede contener letras y numeros"
     }
+    if (giftDescription.length > 100) {
+        return "Este campo no puede tener mas de 100 caracteres"
+    }
+    return ""
 }
 
 
@@ -42,5 +33,49 @@ function validateTown (town) {
         return "Seleccione su locación"
     }
     
+    return ""
 }
+
+function handleMistakes (mistakes){
+    const keys = Object.keys(mistakes)
+    console.log(keys)
+
+    keys.forEach(function(key){
+        const mistake = mistakes[key]
+        const $mistakes = document.querySelector("#mistakes")
+
+        if(mistake){
+            $form[key].className = "mistake"
+
+            const $mistake = document.createElement("li")
+            $mistake.innerText = mistake
+            $mistakes.appendChild($mistake)
+        }else{
+            $form[key].className = ""
+        }
+    
+    })
+}
+
+$form.onsubmit = function validateForm(event){
+    const $name = $form.name.value
+    const city = $form.city.value
+    const giftDescription = $form["gift-description"].value
+
+    const mistakeName = validateName($name)
+    const mistakeCity = validateCity(city)
+    const mistakeGiftDescription = validateGiftDescription(giftDescription)
+
+    const mistakes = {
+        city: mistakeCity,
+        name: mistakeName,
+        "gift-description": mistakeGiftDescription
+    }
+
+
+    handleMistakes(mistakes)
+    event.preventDefault()
+}
+
+
 
--- js-xmas-edition-master/js/pruebas.js
+++ js-xmas-edition-master/js/pruebas.js
@@ -1,46 +1,51 @@
+
 function tryValidateName() {
-    console.assert(
-        validateName('') === 'Este campo debe tener al menos un caracter',
-        'Validar nombre no validó que el nombre no sea vacío',
-    );
-  
-    console.assert(
-        validateName(
-            '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-             
-        'Este campo no puede tener mas de 50 caracteres',
-        'Validar nombre no validó que el nombre sea menor a 50 caracteres',
-    )
-  }
-  
-  tryValidateName()
-  
-  
-  function  tryValidateTown() {
-    console.assert(
-        validateTown("") === "Seleccione su locación",
-        "Validar ciudad no validó que se seleccione una locación"
-    )
-  } 
-  
+  console.assert(
+    validateName('') === 'Este campo debe tener al menos un caracter',
+    'Validar nombre no validó que el nombre no sea vacío',
+  );
+
+  console.assert(
+      validateName(
+          'llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll') ===
+          "Este campo no puede tener más de 50 caracteres",
+      'validateName no validó que el nombre sea menor a 50 caracteres',
+  )
+  console.assert(
+    validateName("123") === "Este campo solo puede tener letras",
+    "validateName no valido que el nombre solo contenga letras"
+  )
+}
+
+tryValidateName()
+
+function  tryValidateTown() {
+  console.assert(
+    validateTown("") === "Seleccione su locación",
+    "Validar ciudad no validó que se seleccione una locación"
+  )
+} 
+
 tryValidateTown()
 
 
-  function tryValidateGiftDescription() {
-    console.assert(
-        validateGiftDescription("") === "Este campo debe tener al menos un caracter",
-        "Validar descripción regalo no validó que la descripción no sea vacía"
-
+function tryValidateGiftDescription() {
+  console.assert(
+    validateGiftDescription("") === "Este campo debe tener al menos un caracter",
+    "Validar descripción regalo no validó que la descripción no sea vacía"
   )
 
-  console.assert(
-      validateGiftDescription(
-          "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII") ===
+console.assert(
+  validateGiftDescription(
+    "IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII") ===
 
-          "Este campo no puede tener mas de 200 caracteres",
-          'Validar nombre no validó que el nombre sea menor a 200 caracteres',
-  )                            
-  }
-  
-  tryValidateGiftDescription()
-  
+    "Este campo no puede tener mas de 100 caracteres",
+    'Validar nombre no validó que el nombre sea menor a 100 caracteres',
+  )      
+  console.assert(
+    validateGiftDescription(",.-") === "Este campo solo puede contener letras y numeros",
+    "validateGifDescription no valido que la descripcion solo tenga letras y numeros"
+  )                      
+}
+
+tryValidateGiftDescription()
